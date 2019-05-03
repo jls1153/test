3594fd4b1944ac2ecdfbdd4b6bbf2b04e2fbb371
jls1153 <jls1153@comcast.net>
2019-05-03 16:27:24 -0400

    Initial commit

--- appendfile.js
+++ appendfile.js
@@ -1,6 +1,9 @@
+/*
+* create a new file with appendFile
+*/
 var fs = require('fs');
 
 fs.appendFile('mynewfile1.txt', 'Hello content!', function (err) {
   if (err) throw err;
-  console.log('Saved!');
+  console.log('Saved!'+"\n");
 });
